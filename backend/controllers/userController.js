d6316e4f383dba60ed2e38989fc3ced3fefa0003
Fahmi Aziz <fahmiaziz031102@gmail.com>
2023-08-15 05:17:24 +0700

    backend yang akan digunakan untuk aplikasi todo list

---
+++ backend/controllers/userController.js
@@ -0,0 +1,60 @@
+import { response } from 'express';
+import User from '../models/userModel.js';
+
+export const getUser = async (req, res) => {
+    try {
+        const respone = await User.findAll();
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const getUserById = async (req, res) => {
+    try {
+        const respone = await User.findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const createUser = async (req, res) => {
+    try {
+        await User.create(req.body);
+        res.status(201).json({msg:"user dibuat"});
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const updateUser = async (req, res) => {
+    try {
+        await User.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ msg: "user diupdate" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    try {
+        await User.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({msg:"user dihapus"});
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import userRoute from './routes/userRoute.js';
+
+
+const app = express();
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(userRoute);
+
+app.listen(5000, () => {
+    console.log("Server Running...");
+})
